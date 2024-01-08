2b156ffe9ba09e9c59a478a5c315e07417d977c6
endb1228 <85601916+endb1228@users.noreply.github.com>
2024-01-08 22:57:31 +0900

    Feat: 프로젝트 생성 및 초기화

---
+++ src/main/java/com/climbing/ClimbingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.climbing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/climbing/climbingbackend/ClimbingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.climbing.climbingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
