0e5e3554231c534c1abe058ee9f6d6c5ef3af563
Kyumin-Chang <kyumin0529@naver.com>
2024-01-29 01:11:06 +0900

    chore: 이메일로 사용자 찾기로 변경

--- src/main/java/com/climbing/domain/member/service/MemberService.java
+++ src/main/java/com/climbing/domain/member/service/MemberService.java
@@ -7,11 +7,11 @@ import com.climbing.domain.member.dto.MemberUpdateDto;
 public interface MemberService {
     void signUp(MemberSignUpDto memberSignUpDto) throws Exception;
 
-    void update(MemberUpdateDto memberUpdateDto, String nickName) throws Exception;
+    void update(MemberUpdateDto memberUpdateDto, String email) throws Exception;
 
-    void updatePassword(String beforePassword, String afterPassword, String nickName) throws Exception;
+    void updatePassword(String beforePassword, String afterPassword, String email) throws Exception;
 
-    void withdraw(String checkPassword, String nickName) throws Exception;
+    void withdraw(String checkPassword, String email) throws Exception;
 
     MemberDto getInfo(Long id) throws Exception;
 
