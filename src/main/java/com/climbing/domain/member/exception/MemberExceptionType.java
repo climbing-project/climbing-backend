cd64d750b509b8868d7180b579560e3a26b20d41
Kyumin-Chang <kyumin0529@naver.com>
2024-01-29 23:04:23 +0900

    chore: Member 예외처리 닉네임 중복 예외 추가

--- src/main/java/com/climbing/domain/member/exception/MemberExceptionType.java
+++ src/main/java/com/climbing/domain/member/exception/MemberExceptionType.java
@@ -5,8 +5,10 @@ import org.springframework.http.HttpStatus;
 
 public enum MemberExceptionType implements BaseExceptionType {
     ALREADY_EXIST_EMAIL(600, HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
-    WRONG_PASSWORD(601, HttpStatus.BAD_REQUEST, "아이디 또는 비밀번호를 잘못 입력했습니다."),
-    NOT_FOUND_MEMBER(602, HttpStatus.NOT_FOUND, "일치하는 회원이 존재하지 않습니다.");
+
+    ALREADY_EXIST_NICKNAME(601, HttpStatus.CONFLICT, "이미 존재하는 닉네임입니다."),
+    WRONG_PASSWORD(602, HttpStatus.BAD_REQUEST, "아이디 또는 비밀번호를 잘못 입력했습니다."),
+    NOT_FOUND_MEMBER(603, HttpStatus.NOT_FOUND, "일치하는 회원이 존재하지 않습니다.");
 
     private final int errorCode;
     private final HttpStatus httpStatus;
