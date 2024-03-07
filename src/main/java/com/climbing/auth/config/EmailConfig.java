27dfaa65c689cdbab01d30bd0fa4abaca530e04d
Kyumin-Chang <kyumin0529@naver.com>
2024-03-07 19:36:20 +0900

    chore : 암호키 secret 처리

--- src/main/java/com/climbing/auth/config/EmailConfig.java
+++ src/main/java/com/climbing/auth/config/EmailConfig.java
@@ -1,5 +1,6 @@
 package com.climbing.auth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,13 +11,19 @@ import java.util.Properties;
 @Configuration
 public class EmailConfig {
 
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 
         javaMailSender.setHost("smtp.gmail.com");
-        javaMailSender.setUsername(System.getenv("GOOGLE_MAIL_USERNAME"));
-        javaMailSender.setPassword(System.getenv("GOOGLE_MAIL_PASSWORD"));
+        javaMailSender.setUsername(username);
+        javaMailSender.setPassword(password);
         javaMailSender.setPort(587);
         javaMailSender.setJavaMailProperties(getProperties());
 
